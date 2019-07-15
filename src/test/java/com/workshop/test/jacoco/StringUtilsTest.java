7d9ae4af59bcad380f6609be9230a6bc3ca44341
Yunzhi Liu <yzliu@thoughtworks.com>
2019-07-15 17:58:00 +0800

    test: add StringUtilsTest

---
+++ src/test/java/com/workshop/test/jacoco/StringUtilsTest.java
@@ -0,0 +1,96 @@
+package com.workshop.test.jacoco;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class StringUtilsTest {
+
+    private StringUtils stringUtils;
+
+    @BeforeEach
+    void setUp() {
+        stringUtils = new StringUtils("");
+    }
+
+    @Test
+    void should_return_true_giving_an_empty_string_when_checking_empty() {
+
+        boolean result = stringUtils.isEmpty("");
+
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    void should_return_true_giving_nul_when_checking_empty() {
+
+        boolean result = stringUtils.isEmpty(null);
+
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    void should_return_false_giving_A_when_checking_empty() {
+
+        boolean result = stringUtils.isEmpty("a");
+
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    void should_return_true_giving_null_when_checking_blank() {
+
+        boolean result = stringUtils.isBlank(null);
+
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    void should_return_true_giving_an_empty_string_when_checking_blank() {
+
+        boolean result = stringUtils.isBlank("");
+
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    void should_return_false_giving_A_when_checking_blank() {
+
+        boolean result = stringUtils.isBlank("a");
+
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    void should_return_true_giving_a_space_when_checking_blank() {
+
+        boolean result = stringUtils.isBlank(" ");
+
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    void should_return_false_giving_a_empty_string_when_checking_alpha() {
+
+        boolean result = stringUtils.isAlpha("");
+
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    void should_return_false_giving_a_space_string_when_checking_alpha() {
+
+        boolean result = stringUtils.isAlpha(" ");
+
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    void should_return_true_giving_A_string_when_checking_alpha() {
+
+        boolean result = stringUtils.isAlpha("a");
+
+        assertThat(result).isTrue();
+    }
+}
